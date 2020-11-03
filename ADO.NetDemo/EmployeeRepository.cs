16955f8aef2fee36e0af75d8b1f27f8aa8d6df40
Rachit Rathi <rachitrathi98@gmail.com>
2020-11-03 20:00:15 +0530

    [Rachit] Add. Initial Commit

---
+++ ADO.NetDemo/EmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO.NetDemo
+{
+    class EmployeeRepository
+    {
+        public static string ConnectionString = "Server=;Initial Catalog=;User ID=;Password=";
+
+    }
+}
